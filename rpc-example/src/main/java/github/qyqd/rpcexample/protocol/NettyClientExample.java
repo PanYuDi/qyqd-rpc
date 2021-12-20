e2dc508b5610017522dfff3a3712285a28b2bb62
PanYuDi <826941817@qq.com>
2021-12-20 19:35:45 +0800

    feat:同步返回测试成功，下一步开始做远程方法调用

--- rpc-example/src/main/java/github/qyqd/rpcexample/protocol/NettyClientExample.java
+++ rpc-example/src/main/java/github/qyqd/rpcexample/protocol/NettyClientExample.java
@@ -1,11 +1,15 @@
 package github.qyqd.rpcexample.protocol;
 
 import github.qyqd.common.enums.ProtocolMessageTypeEnum;
+import github.qyqd.rpc.remote.RequestMessage;
 import github.qyqd.rpc.remote.RpcClient;
 import github.qyqd.rpc.remote.message.ProtocolMessage;
 import github.qyqd.rpc.remote.transport.netty.client.NettyClient;
 import github.qyqd.rpc.remote.transport.netty.request.ProtocolRequestEndpointWrapper;
 import github.qyqd.rpc.remote.utils.ProtocolMessageUtils;
+
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
 
 /**
  * @ClassName NettyClientExample
@@ -15,13 +19,16 @@ import github.qyqd.rpc.remote.utils.ProtocolMessageUtils;
  * Version 1.0
  */
 public class NettyClientExample {
-    public static void main(String[] args) {
-        RpcClient rpcClient = new NettyClient(1000L);
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+        RpcClient rpcClient = new NettyClient(1000 * 1000 * 1000L);
         ProtocolRequestEndpointWrapper wrapper = new ProtocolRequestEndpointWrapper();
         wrapper.setHost("127.0.0.1");
         wrapper.setPort(8080);
         wrapper.setMessageTypeEnum(ProtocolMessageTypeEnum.HEARTBEAT_REQUEST_MESSAGE);
         wrapper.setRequestBody(ProtocolMessageUtils.createHeartBeatRequestMessage());
-        rpcClient.send(wrapper);
+        wrapper.setRequestId(ProtocolMessageUtils.generateRequestId());
+        CompletableFuture<RequestMessage> send = (CompletableFuture<RequestMessage>) rpcClient.send(wrapper);
+        RequestMessage x = send.get();
+        System.out.println(x);
     }
 }
